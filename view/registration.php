6ce818eaaac15b1e631facab79d69c7b857ce926
ProgrammerLori <fustos.lorant.2017ice@bankitatabanya.hu>
2022-02-02 16:27:28 +0100

    viewok

---
+++ view/registration.php
@@ -0,0 +1,16 @@
+<?php
+ include 'includes/db.inc.php' 
+
+?>
+
+<table>
+
+<form action="registrate.php" method="post" value="registration">
+    <?php
+echo "<h2>Regisztráció</h2>"
+?>
+							Felhasználó:<br><input type="text" name="user">
+							<br>
+							Jelszó: <br><input type="password" name="passw">
+							<br>
+						<input type="submit">
