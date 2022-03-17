5f8e1a1c03ede6266b11b412f2d10e2dc4a0a655
ProgrammerLori <fustos.lorant.2017ice@bankitatabanya.hu>
2022-03-17 07:12:56 +0100

    updo

--- controller/index.php
+++ controller/index.php
@@ -1,4 +1,86 @@
-
 <?php
+// Gombnyomásra belép
+if(isset($_POST['upvote'])){
+    // Megnézi hogy ez a felhasználó upvoteolta-e már ezt a képet
+    $sql = "SELECT users_id FROM likes WHERE vote = 0 and liked_pic_id=".$_POST['voteid']." and users_id=".$_SESSION['users_id']."";
+    if(!$result = $conn->query($sql)) echo $conn->error;
+    //ha nem akkor megnézi hogy felhasználó downvoteolta-e már ezt a képet
+    if(!$result->num_rows > 0){
+        $sql = "SELECT users_id FROM likes WHERE vote = 1 and liked_pic_id=".$_POST['voteid']." and users_id=".$_SESSION['users_id']."";
+        if(!$result = $conn->query($sql)) echo $conn->error;
+        // ha igen kitörli
+        if($result->num_rows > 0){
+            $sql = "UPDATE  likes SET vote=0 WHERE vote = 1 and users_id=".$_SESSION['users_id']." and liked_pic_id= ".$_POST['voteid']."";
+            
+            if(!$result = $conn->query($sql)) echo $conn->error;
+            if ($conn->query($sql) === TRUE) {
+            } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+            //utána beszúrja az adatbázisba
+        }else{
+            $sql = "INSERT INTO likes (users_id,liked_pic_id,vote)
+            VALUES ('".$_SESSION['users_id']."','".($_POST['voteid'])."','0')";
+             if ($conn->query($sql) === TRUE) {
+                header('Location: index.php?page=index');
+            } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+        }
+
+    }else{
+        $sql = "DELETE FROM likes WHERE vote = 0 and users_id=".$_SESSION['users_id']." and liked_pic_id= ".$_POST['voteid']."";
+        
+        
+        if(!$result = $conn->query($sql)) echo $conn->error;
+            if ($conn->query($sql) === TRUE) {
+                header('Location: index.php?page=index');
+            } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+     }
+    
+}
+if(isset($_POST['downvote'])){
+    // Megnézi hogy ez a felhasználó upvoteolta-e már ezt a képet
+    $sql = "SELECT users_id FROM likes WHERE vote = 1 and liked_pic_id=".$_POST['voteid']." and users_id=".$_SESSION['users_id']."";
+    if(!$result = $conn->query($sql)) echo $conn->error;
+    //ha nem akkor megnézi hogy felhasználó downvoteolta-e már ezt a képet
+    if(!$result->num_rows > 0){
+        $sql = "SELECT users_id FROM likes WHERE vote = 0 and liked_pic_id=".$_POST['voteid']." and users_id=".$_SESSION['users_id']."";
+        if(!$result = $conn->query($sql)) echo $conn->error;
+        // ha igen kitörli
+        if($result->num_rows > 0){
+            $sql = "UPDATE  likes SET vote=1 WHERE vote = 0 and users_id=".$_SESSION['users_id']." and liked_pic_id= ".$_POST['voteid']."";
+            if(!$result = $conn->query($sql)) echo $conn->error;
+            if ($conn->query($sql) === TRUE) {
+            } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+            //utána beszúrja az adatbázisba
+        }else{
+            $sql = "INSERT INTO likes (users_id,liked_pic_id,vote)
+            VALUES ('".$_SESSION['users_id']."','".($_POST['voteid'])."','1')";
+             if ($conn->query($sql) === TRUE) {
+                header('Location: index.php?page=index');
+            } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+        }
+
+    }else{
+        $sql = "DELETE FROM likes WHERE vote = 1 and users_id=".$_SESSION['users_id']." and liked_pic_id= ".$_POST['voteid']."";
+        
+        
+        if(!$result = $conn->query($sql)) echo $conn->error;
+            if ($conn->query($sql) === TRUE) {
+                header('Location: index.php?page=index');
+            } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+     }
+    
+}
+
 include 'view/index.php';
 ?>
--- view/profile.php
+++ view/profile.php
@@ -1,12 +1,20 @@
 <?php 
-
+if ($i>0) echo "$i fájl feltöltve" ;
+if ($errors) {
+    foreach($errors as $error){
+        foreach($error as $errorMsg){
+            echo"$errorMsg <br>";
+        
+        }
+    }
+}
 echo'<img id="profilkep" src="'.$_SESSION['picture_name'].'"><br>';?>
 
 <form action="index.php?page=profile" method="POST" enctype="multipart/form-data">
         Profilkép megváltoztatása:
         <input type="file" name="profilepic" id="profilepic" >
         <br>
-        <input type="submit">
+        <input type="submit" name="profilepicbutton">
         
 </form>
 <?php
@@ -16,7 +24,7 @@ if ($_SESSION["gender"]==0) {
     echo "Nem: Férfi";
 }else echo "Nem: Nő";
 
-echo"<div class='flex-container'>";
+echo"<div class='flex-container' id='flex-con'>";
 if ($pictureIds) {
 
     foreach($pictureIds as $pictureId) {
@@ -30,11 +38,8 @@ if ($pictureIds) {
             
                 if($pictures->get_category()!="Profilkep"){
                     if ($pictures->get_users_id()==$_SESSION['users_id']) {
-                        echo '<div class="keret"><span><img class="kepek" src="'.$pictures->get_picture_name().'"><span>'.$pictures->get_category().'</span></span></div>';
+                        echo '<div class="p_keret"><span><img class="kepek" src="'.$pictures->get_picture_name().'"><span>'.$pictures->get_category().'</span></span></div>';
                     }
-                    
-            
-            
         }
         
            
